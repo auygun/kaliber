ce45f8c95d5ab789f804848fb1e2071220b68419
Attila Uygun <auygun@opera.com>
2020-04-24 11:00:22 +0200

    Fix accumulate time.

--- src/platform/platform.cc
+++ src/platform/platform.cc
@@ -19,46 +19,41 @@ void Platform::RunMainLoop() {
   }
 
   // Use fixed time steps.
-  constexpr float time_step = 1.0f / 30.0f;
+  constexpr float time_step = 1.0f / 60.0f;
   constexpr float speed = 1.0f;
+  constexpr float epsilon = 0.000001f;
 
   Timer timer;
   float last_time = timer.GetSecondsAccumulated();
   float accumulator = 0.0;
   float frame_frac = 0.0f;
 
-  bool should_draw = true;
   for (;;) {
-    if (should_draw) {
-      // LOG("Draw!!!\n");
-      should_draw = false;
-      engine::Engine::Get().Draw(frame_frac);
+    engine::Engine::Get().Draw(frame_frac);
 
-      Update();
-      if (should_exit_) {
-        engine::Engine::Get().Shutdown();
-        return;
+    Update();
+    if (should_exit_) {
+      engine::Engine::Get().Shutdown();
+      return;
+    }
+
+    // Accumulate time.
+    while (accumulator < time_step) {
+      timer.Update();
+      float new_time = timer.GetSecondsAccumulated();
+      accumulator += new_time - last_time;
+      last_time = new_time;
+      if (time_step - accumulator > epsilon) {
+        float sleep_time = time_step - accumulator - epsilon;
+        std::this_thread::sleep_for(std::chrono::microseconds((int)(sleep_time * 1000000.0f)));
+        accumulator += sleep_time;
       }
-    }
-
-    timer.Update();
-    float new_time = timer.GetSecondsAccumulated();
-    accumulator += new_time - last_time;
-    last_time = new_time;
-    // LOG("accumulator: %f\n", accumulator);
-
-    if (accumulator < time_step) {
-      float sleep_time = time_step - accumulator;
-      // LOG("sleep for: %f\n", sleep_time);
-      std::this_thread::sleep_for(std::chrono::nanoseconds((int)(sleep_time * 1000000000.0f)));
-      accumulator += sleep_time;
-    }
+    };
 
     // Subdivide the frame time.
     while (accumulator >= time_step) {
       engine::Engine::Get().Update(time_step * speed);
       accumulator -= time_step;
-      should_draw = true;
     };
 
     // Calculate frame fraction from remainder of the frame time.
