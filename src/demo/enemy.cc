1f4d31570ddaa8594efabc5db4dcc70f6f44fb65
Attila Uygun <auygun@opera.com>
2020-05-04 21:57:30 +0200

    UnitType

--- src/demo/enemy.cc
+++ src/demo/enemy.cc
@@ -6,11 +6,14 @@
 #include "../engine/engine.h"
 #include <memory>
 #include <limits>
+#include <cassert>
 
 bool Enemy::Initialize() {
   engine::Engine& engine = engine::Engine::Get();
-  enemy_frames_ = engine.GetAssetManager().GetImage(
+  skull_frames_ = engine.GetAssetManager().GetImage(
       "enemy_anims_01_frames_ok.png");
+  bug_frames_ = engine.GetAssetManager().GetImage(
+      "enemy_anims_02_frames_ok.png");
   target_frames_ = engine.GetAssetManager().GetImage(
       "enemy_target_single_ok.png");
   blast_frames_ = engine.GetAssetManager().GetImage(
@@ -24,13 +27,16 @@ void Enemy::Update(float delta_time) {
   seconds_since_last_spawn_ += delta_time;
   if (seconds_since_last_spawn_ >= 1) {
     seconds_since_last_spawn_ = 0;
-    DamageType type = (DamageType)(RandomInt() % kDamageType_Max);
+
     Vector2 s = engine.GetScreenSize();
     float col = (float)(RandomInt() % 5);
     float x = (s.x / 5) / 2 + (s.x / 5) * col - s.x / 2;
     Vector2 pos = {x, s.y / 2};
     float speed = RandomFloat();
-    Spawn(type, pos, speed);
+
+    UnitType unit_type = (UnitType)(RandomInt() % kUnitType_Max);
+    DamageType damage_type = (DamageType)(RandomInt() % kDamageType_Max);
+    Spawn(unit_type, damage_type, pos, speed);
   }
 
   for (auto it = enemies_.begin(); it != enemies_.end(); ++ it) {
@@ -45,18 +51,18 @@ void Enemy::Update(float delta_time) {
   }
 }
 
-bool Enemy::HasTarget(DamageType type) {
-  return GetTarget(type) ? true : false;
+bool Enemy::HasTarget(DamageType damage_type) {
+  return GetTarget(damage_type) ? true : false;
 }
 
-Vector2 Enemy::GetTargetPos(DamageType type) {
-  Unit *target = GetTarget(type);
+Vector2 Enemy::GetTargetPos(DamageType damage_type) {
+  Unit *target = GetTarget(damage_type);
   if (target)
     return target->sprite.offset() - Vector2(0, target->sprite.scale().y / 2.5f);
   return {0, 0};
 }
 
-void Enemy::SelectTarget(DamageType type,
+void Enemy::SelectTarget(DamageType damage_type,
                          const Vector2& weapon_pos,
                          const Vector2& target_pos) {
   Unit* current_enemy = nullptr;
@@ -69,7 +75,7 @@ void Enemy::SelectTarget(DamageType type,
     if (!e.alive)
       continue;
 
-    if (e.targetted_by_weapon_ == type)
+    if (e.targetted_by_weapon_ == damage_type)
       current_enemy = &e;
 
     Vector2 weapon_enemy_dir = e.sprite.offset() - weapon_pos;
@@ -96,9 +102,9 @@ void Enemy::SelectTarget(DamageType type,
   }
 
   if (best_enemy) {
-    best_enemy->targetted_by_weapon_ = type;
+    best_enemy->targetted_by_weapon_ = damage_type;
     best_enemy->target.SetVisible(true);
-    if (type == kDamageType_Green)
+    if (damage_type == kDamageType_Green)
       best_enemy->target_frame_animator.SetFrameRange(0 ,6, 5);
     else
       best_enemy->target_frame_animator.SetFrameRange(6 ,12, 11);
@@ -106,8 +112,8 @@ void Enemy::SelectTarget(DamageType type,
   }
 }
 
-void Enemy::DeselectTarget(DamageType type) {
-  Unit *target = GetTarget(type);
+void Enemy::DeselectTarget(DamageType damage_type) {
+  Unit *target = GetTarget(damage_type);
   if (target) {
     target->targetted_by_weapon_ = kDamageType_Invalid;
     target->target.SetVisible(false);
@@ -115,9 +121,9 @@ void Enemy::DeselectTarget(DamageType type) {
   }
 }
 
-void Enemy::KillTarget(DamageType type) {
-  Unit *target = GetTarget(type);
-  if (!target || target->type != type) {
+void Enemy::KillTarget(DamageType damage_type) {
+  Unit *target = GetTarget(damage_type);
+  if (!target || target->damage_type != damage_type) {
     if (target)
       target->target.SetVisible(false);
     return;
@@ -131,23 +137,40 @@ void Enemy::KillTarget(DamageType type) {
   });
 }
 
-void Enemy::Spawn(DamageType type, const Vector2& pos, float speed) {
+void Enemy::Spawn(UnitType unit_type,
+                  DamageType damage_type,
+                  const Vector2& pos,
+                  float speed) {
+  assert(unit_type > kUnitType_Invalid && unit_type < kUnitType_Max);
+  assert(damage_type > kDamageType_Invalid && damage_type < kDamageType_Max);
+
   engine::Engine& engine = engine::Engine::Get();
   Demo* game = static_cast<Demo*>(engine.GetGame());
 
   auto& e = enemies_.emplace_back();
-  e.type = type;
-  e.sprite.Create(enemy_frames_, {10, 6});
+  e.unit_type = unit_type;
+  e.damage_type = damage_type;
+  if (unit_type == kUnitType_Skull)
+    e.sprite.Create(skull_frames_, {10, 6});
+  else
+    e.sprite.Create(bug_frames_, {10, 4});
   e.sprite.Scale(2);
   e.sprite.SetVisible(true);
   e.sprite.SetOffset(pos);
   engine.AddDrawable(&e.sprite);
 
   e.sprite_frame_animator.AttachFrameController(&e.sprite);
-  if (type == kDamageType_Green)
-    e.sprite_frame_animator.SetFrameRange(0 ,21, 20);
-  else
-    e.sprite_frame_animator.SetFrameRange(30 ,51, 50);
+  if (damage_type == kDamageType_Green) {
+    if (unit_type == kUnitType_Skull)
+      e.sprite_frame_animator.SetFrameRange(0 ,5, 4);
+    else
+      e.sprite_frame_animator.SetFrameRange(13 ,19, 18);
+  } else {
+    if (unit_type == kUnitType_Skull)
+      e.sprite_frame_animator.SetFrameRange(30 ,35, 34);
+    else
+      e.sprite_frame_animator.SetFrameRange(34 ,39, 38);
+  }
   e.sprite_frame_animator.Play(true, true);
 
   e.target.Create(target_frames_, {6, 2});
@@ -165,7 +188,7 @@ void Enemy::Spawn(DamageType type, const Vector2& pos, float speed) {
 
   e.blast_frame_animator.AttachFrameController(&e.blast);
   e.blast_frame_animator.SetSpeed(0.05f);
-  if (type == kDamageType_Green)
+  if (damage_type == kDamageType_Green)
     e.blast_frame_animator.SetFrameRange(0 ,6, 5);
   else
     e.blast_frame_animator.SetFrameRange(6 ,12, 11);
@@ -188,9 +211,9 @@ void Enemy::Spawn(DamageType type, const Vector2& pos, float speed) {
   e.draw_animator.Play(false);
 }
 
-Enemy::Unit* Enemy::GetTarget(DamageType type) {
+Enemy::Unit* Enemy::GetTarget(DamageType damage_type) {
   for (auto& e : enemies_) {
-    if (e.targetted_by_weapon_ == type)
+    if (e.targetted_by_weapon_ == damage_type)
       return &e;
   }
   return nullptr;
--- src/demo/enemy.h
+++ src/demo/enemy.h
@@ -17,40 +17,55 @@ class Enemy {
 
   void Update(float delta_time);
 
-  bool HasTarget(DamageType type);
-  Vector2 GetTargetPos(DamageType type);
+  bool HasTarget(DamageType damage_type);
+  Vector2 GetTargetPos(DamageType damage_type);
 
-  void SelectTarget(DamageType type,
+  void SelectTarget(DamageType damage_type,
                     const Vector2& weapon_pos,
                     const Vector2& target_pos);
-  void DeselectTarget(DamageType type);
-  void KillTarget(DamageType type);
+  void DeselectTarget(DamageType damage_type);
+  void KillTarget(DamageType damage_type);
 
  private:
+  enum UnitType {
+    kUnitType_Invalid = -1,
+    kUnitType_Skull,
+    kUnitType_Bug,
+    kUnitType_Max
+  };
+
   struct Unit {
-    DamageType type = kDamageType_Invalid;
+    UnitType unit_type = kUnitType_Invalid;
+    DamageType damage_type = kDamageType_Invalid;
+
     bool alive = true;
     bool marked_for_removal = false;
     DamageType targetted_by_weapon_ = kDamageType_Invalid;
+
     engine::ImageQuad sprite;
     engine::ImageQuad target;
     engine::ImageQuad blast;
+
     engine::DrawAnimator draw_animator;
     engine::FrameAnimator sprite_frame_animator;
     engine::FrameAnimator target_frame_animator;
     engine::FrameAnimator blast_frame_animator;
   };
 
-  std::shared_ptr<const engine::Image> enemy_frames_;
+  std::shared_ptr<const engine::Image> skull_frames_;
+  std::shared_ptr<const engine::Image> bug_frames_;
   std::shared_ptr<const engine::Image> target_frames_;
   std::shared_ptr<const engine::Image> blast_frames_;
 
   std::list<Unit> enemies_;
   float seconds_since_last_spawn_ = 0;
 
-  void Spawn(DamageType type, const Vector2& pos, float speed);
+  void Spawn(UnitType unit_type,
+             DamageType damage_type,
+             const Vector2& pos,
+             float speed);
 
-  Unit* GetTarget(DamageType type);
+  Unit* GetTarget(DamageType damage_type);
 };
 
 #endif  // ENEMY_H
