31237158f6eee119874db243402fdf89d3c20eaa
Attila Uygun <auygun@opera.com>
2020-05-07 13:28:39 +0200

    Use GL_LINEAR

--- src/engine/renderer/renderer.cc
+++ src/engine/renderer/renderer.cc
@@ -333,8 +333,8 @@ void Renderer::HandleCmdCreateTexture(RenderCommand* cmd) {
   }
 
   if (new_texture) {
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
     texture_map_[c->id] = gl_id;
