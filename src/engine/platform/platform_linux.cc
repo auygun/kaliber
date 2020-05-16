984b683fe68a02fb4d7e83ea50531416160a837b
Attila Uygun <auygun@opera.com>
2020-05-16 23:24:20 +0200

    Linux input events.

--- src/engine/platform/platform_linux.cc
+++ src/engine/platform/platform_linux.cc
@@ -1,4 +1,5 @@
 #include "../../base/log.h"
+#include "../../base/vecmath.h"
 #include "../engine.h"
 #include "../input_event.h"
 #include "../renderer/renderer.h"
@@ -26,25 +27,60 @@ void Platform::Initialize() {
 
   Display* display = renderer_->display();
   Window window = renderer_->window();
+  XSelectInput(display, window, KeyPressMask | Button1MotionMask |
+      ButtonPressMask | ButtonReleaseMask);
   Atom WM_DELETE_WINDOW = XInternAtom(display, "WM_DELETE_WINDOW", false);
   XSetWMProtocols(display, window, &WM_DELETE_WINDOW, 1);
 }
 
 void Platform::Update() {
   Display* display = renderer_->display();
-  if (!XPending(display))
-    return;
-  XEvent e;
-  XNextEvent(display, &e);
-  if (e.type == KeyPress) {
-    KeySym key = XLookupKeysym(&e.xkey, 0);
-    auto input_event = std::make_unique<InputEvent>(InputEvent::kKeyPress, key);
-    Engine::Get().AddInputEvent(std::move(input_event));
-    // TODO: e.xkey.state & (ShiftMask | ControlMask | Mod1Mask | Mod4Mask))
-  } else if (e.type == ClientMessage) {
-    // TODO: Should check here for other client message types. However the only
-    // protocol registered above is WM_DELETE_WINDOW for now.
-    should_exit_ = true;
+  while (XPending(display)) {
+    XEvent e;
+    XNextEvent(display, &e);
+    switch (e.type) {
+    case KeyPress: {
+      KeySym key = XLookupKeysym(&e.xkey, 0);
+      auto input_event = std::make_unique<InputEvent>(InputEvent::kKeyPress, key);
+      Engine::Get().AddInputEvent(std::move(input_event));
+      // TODO: e.xkey.state & (ShiftMask | ControlMask | Mod1Mask | Mod4Mask))
+      break;
+    }
+    case MotionNotify: {
+      Vector2 v(e.xmotion.x, e.xmotion.y);
+      v = Engine::Get().ToPosition(v);
+      // DLOG << "drag: " << v;
+      auto input_event = std::make_unique<InputEvent>(
+          InputEvent::kDrag, v * Vector2(1, -1));
+      Engine::Get().AddInputEvent(std::move(input_event));
+      break;
+    }
+    case ButtonPress: {
+      if (e.xbutton.button == 1) {
+        Vector2 v(e.xbutton.x, e.xbutton.y);
+        v = Engine::Get().ToPosition(v);
+        // DLOG << "drag-start: " << v;
+        auto input_event = std::make_unique<InputEvent>(
+            InputEvent::kDragStart, v * Vector2(1, -1));
+        Engine::Get().AddInputEvent(std::move(input_event));
+      }
+      break;
+    }
+    case ButtonRelease: {
+      if (e.xbutton.button == 1) {
+        // DLOG << "drag-end!";
+        auto input_event = std::make_unique<InputEvent>(
+            InputEvent::kDragEnd);
+        Engine::Get().AddInputEvent(std::move(input_event));
+      }
+      break;
+    }
+    case ClientMessage: {
+      // TODO: Should check here for other client message types. However the only
+      // protocol registered above is WM_DELETE_WINDOW for now.
+      should_exit_ = true;
+      break;
+    }}
   }
 }
 
