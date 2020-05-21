176bf39016674e4ab30490e5ef69ef58b0424431
Attila Uygun <auygun@opera.com>
2020-05-21 12:09:04 +0200

    Fix intersection test

--- src/base/collusion_test.cc
+++ src/base/collusion_test.cc
@@ -1,28 +1,32 @@
 #include "collusion_test.h"
 
+#include <algorithm>
+#include <limits>
+
 namespace base {
 
-bool Intersection(Vector2 center,
-                         Vector2 size,
-                         Vector2 origin,
-                         Vector2 dir) {
+bool Intersection(Vector2 center, Vector2 size, Vector2 origin, Vector2 dir) {
   Vector2 min = center - size / 2;
   Vector2 max = center + size / 2;
 
-  float r_dir_inv_x = 1.0f / dir.x;
-  float r_dir_inv_y = 1.0f / dir.y;
+  float tmin = std::numeric_limits<float>::min();
+  float tmax = std::numeric_limits<float>::max();
 
-  float tx1 = (min.x - origin.x) * r_dir_inv_x;
-  float tx2 = (max.x - origin.x) * r_dir_inv_x;
+  if (dir.x != 0.0) {
+    float tx1 = (min.x - origin.x) / dir.x;
+    float tx2 = (max.x - origin.x) / dir.x;
 
-  float tmin = std::min(tx1, tx2);
-  float tmax = std::max(tx1, tx2);
+    tmin = std::max(tmin, std::min(tx1, tx2));
+    tmax = std::min(tmax, std::max(tx1, tx2));
+  }
 
-  float ty1 = (min.y - origin.y) * r_dir_inv_y;
-  float ty2 = (max.y - origin.y) * r_dir_inv_y;
+  if (dir.y != 0.0) {
+    float ty1 = (min.y - origin.y) / dir.y;
+    float ty2 = (max.y - origin.y) / dir.y;
 
-  tmin = std::max(tmin, std::min(ty1, ty2));
-  tmax = std::min(tmax, std::max(ty1, ty2));
+    tmin = std::max(tmin, std::min(ty1, ty2));
+    tmax = std::min(tmax, std::max(ty1, ty2));
+  }
 
   return tmax >= tmin;
 }
