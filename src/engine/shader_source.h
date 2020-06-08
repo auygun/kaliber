0ff5a8b3084397905dc7324c3257dcdd0ecee05d
Attila Uygun <auygun@opera.com>
2020-06-08 11:50:54 +0200

    Use unique_ptr to avoid memcpy in shader source.

--- src/engine/shader_source.h
+++ src/engine/shader_source.h
@@ -1,6 +1,7 @@
 #ifndef SHADER_CODE_H
 #define SHADER_CODE_H
 
+#include <memory>
 #include <string>
 #include "asset.h"
 
@@ -13,12 +14,12 @@ class ShaderSource : public Asset {
 
   bool Load(const std::string& name) override;
 
-  const std::string& GetVertexSource() const { return vertex_source_; }
-  const std::string& GetFragmentSource() const { return fragment_source_; }
+  const char* GetVertexSource() const { return vertex_source_.get(); }
+  const char* GetFragmentSource() const { return fragment_source_.get(); }
 
  private:
-  std::string vertex_source_;
-  std::string fragment_source_;
+  std::unique_ptr<char[]> vertex_source_;
+  std::unique_ptr<char[]> fragment_source_;
 };
 
 }  // namespace eng
