0c48942e9f05c5f61a3f1965d12be8a3ba427572
Attila Uygun <auygun@opera.com>
2020-06-16 02:44:43 +0200

    Fix linux build

--- src/engine/audio/audio_null.h
+++ src/engine/audio/audio_null.h
@@ -1,7 +1,11 @@
 #ifndef AUDIO_NULL_H
 #define AUDIO_NULL_H
 
+#include <memory>
+
 namespace eng {
+
+class Sound;
 
 class AudioNull {
  public:
@@ -11,6 +15,15 @@ class AudioNull {
   bool Initialize() { return true; }
 
   void Shutdown() {}
+
+  std::shared_ptr<AudioResource> CreateResource() { return nullptr; }
+
+  void Play(std::shared_ptr<const Sound> sound,
+            std::shared_ptr<void> impl_data,
+            bool loop,
+            size_t step) {}
+
+  void Stop(std::shared_ptr<void> impl_data) {}
 };
 
 }  // namespace eng
