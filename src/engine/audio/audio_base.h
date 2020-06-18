d4839ce0087f3458f719c6ed6de1e2aa92cccb09
Attila Uygun <auygun@opera.com>
2020-06-18 11:22:47 +0200

    AudioBase

---
+++ src/engine/audio/audio_base.h
@@ -0,0 +1,58 @@
+#ifndef AUDIO_BASE_H
+#define AUDIO_BASE_H
+
+#include <memory>
+#include <mutex>
+#include <list>
+
+namespace eng {
+
+class Sound;
+
+class AudioBase {
+ public:
+  AudioBase();
+  ~AudioBase();
+
+  void Play(std::shared_ptr<void> impl_data,
+            std::shared_ptr<const Sound> sound,
+            float amplitude,
+            bool reset_pos);
+
+  void Stop(std::shared_ptr<void> impl_data);
+
+  void SetLoop(std::shared_ptr<void> impl_data, bool loop);
+  void SetSimulateStereo(std::shared_ptr<void> impl_data, bool simulate);
+  void SetResampleStep(std::shared_ptr<void> impl_data, size_t step);
+  void SetMaxAmplitude(std::shared_ptr<void> impl_data, float max_amplitude);
+  void SetAmplitudeInc(std::shared_ptr<void> impl_data, float amplitude_inc);
+
+ protected:
+  enum SampleFlags { kLoop = 1, kStopped = 2, kSimulateStereo = 4 };
+
+  static constexpr int kChannelCount = 2;
+
+  struct Sample {
+    // Read-only accessed by the audio thread.
+    std::shared_ptr<const Sound> sound;
+    unsigned flags = 0;
+    size_t step = 10;
+    float amplitude_inc = 0;
+    float max_amplitude = 1.0f;
+
+    // Write accessed by the audio thread.
+    size_t src_index = 0;
+    size_t accumulator = 0;
+    float amplitude = 1.0f;
+    bool active = false;
+  };
+
+  std::list<std::shared_ptr<Sample>> samples_[2];
+  std::mutex mutex_;
+
+  void RenderAudio(float *output_buffer, size_t num_frames);
+};
+
+}  // namespace eng
+
+#endif  // AUDIO_BASE_H
--- src/engine/audio/audio_oboe.cc
+++ src/engine/audio/audio_oboe.cc
@@ -1,21 +1,10 @@
 #include "audio_oboe.h"
 
-#include <cstring>
-
-#include "../../base/interpolation.h"
 #include "../../base/log.h"
-#include "../../base/random.h"
 #include "../../third_party/oboe/include/oboe/Oboe.h"
-#include "../sound.h"
 #include "audio_resource.h"
 
 using namespace base;
-
-namespace {
-
-constexpr int kChannelCount = 2;
-
-}  // namespace
 
 namespace eng {
 
@@ -62,154 +51,8 @@ std::shared_ptr<AudioResource> AudioOboe::CreateResource() {
   return std::make_shared<AudioResource>(impl_data, this);
 }
 
-void AudioOboe::Play(std::shared_ptr<void> impl_data,
-                     std::shared_ptr<const Sound> sound,
-                     float amplitude,
-                     bool reset_pos) {
-  auto sample = std::static_pointer_cast<Sample>(impl_data);
-  if (sample->active)
-    return;
-
-  if (reset_pos) {
-    sample->src_index = 0;
-    sample->accumulator = 0;
-  }
-  sample->flags &= ~kStopped;
-  sample->sound = sound;
-  sample->amplitude = amplitude;
-  sample->active = true;
-
-  std::unique_lock<std::mutex> scoped_lock(mutex_);
-  samples_[0].push_back(sample);
-}
-
-void AudioOboe::Stop(std::shared_ptr<void> impl_data) {
-  auto sample = std::static_pointer_cast<Sample>(impl_data);
-  if (!sample->active)
-    return;
-
-  sample->flags |= kStopped;
-}
-
-void AudioOboe::SetLoop(std::shared_ptr<void> impl_data, bool loop) {
-  auto sample = std::static_pointer_cast<Sample>(impl_data);
-  if (loop)
-    sample->flags |= kLoop;
-  else
-    sample->flags &= ~kLoop;
-}
-
-void AudioOboe::SetSimulateStereo(std::shared_ptr<void> impl_data,
-                                  bool simulate) {
-  auto sample = std::static_pointer_cast<Sample>(impl_data);
-  if (simulate)
-    sample->flags |= kSimulateStereo;
-  else
-    sample->flags &= ~kSimulateStereo;
-}
-
-void AudioOboe::SetResampleStep(std::shared_ptr<void> impl_data, size_t step) {
-  auto sample = std::static_pointer_cast<Sample>(impl_data);
-  sample->step = step + 10;
-}
-
-void AudioOboe::SetMaxAmplitude(std::shared_ptr<void> impl_data, float max_amplitude) {
-  auto sample = std::static_pointer_cast<Sample>(impl_data);
-  sample->max_amplitude = max_amplitude;
-}
-
-void AudioOboe::SetAmplitudeInc(std::shared_ptr<void> impl_data,
-                                float amplitude_inc) {
-  auto sample = std::static_pointer_cast<Sample>(impl_data);
-  sample->amplitude_inc = amplitude_inc;
-}
-
 size_t AudioOboe::GetSampleRate() {
   return stream_->getSampleRate();
-}
-
-void AudioOboe::RenderAudio(float *output_buffer, int32_t num_frames) {
-  {
-    std::unique_lock<std::mutex> scoped_lock(mutex_);
-    samples_[1].splice(samples_[1].end(), samples_[0]);
-  }
-
-  memset(output_buffer, 0, sizeof(float) * num_frames * kChannelCount);
-
-  for (auto it = samples_[1].begin(); it != samples_[1].end();) {
-    Sample* sample = it->get();
-
-    unsigned flags = sample->flags;
-    bool remove = false;
-
-    if (flags & kStopped) {
-      remove = true;
-    } else {
-      const float *src[2] = {sample->sound->GetBuffer(0),
-                            sample->sound->GetBuffer(1)};
-      if (!src[1])
-        src[1] = src[0];
-      size_t num_samples = sample->sound->num_samples();
-      size_t num_channels = sample->sound->num_channels();
-      size_t src_index = sample->src_index;
-      size_t step = sample->step;
-      size_t accumulator = sample->accumulator;
-      float amplitude = sample->amplitude;
-      float amplitude_inc = sample->amplitude_inc;
-      float max_amplitude = sample->max_amplitude;
-
-      size_t channel_offset = (flags & kSimulateStereo) && num_channels == 1
-                              ? sample->sound->hz() / 10
-                              : 0;
-
-      for (size_t i = 0; i < num_frames * kChannelCount;) {
-        // Mix the 1st channel.
-        output_buffer[i++] += src[0][src_index] * amplitude;
-
-        // Mix the 2nd channel. Offset the source index for stereo simulation.
-        size_t ind = channel_offset + src_index;
-        if (ind < num_samples)
-          output_buffer[i++] += src[1][ind] * amplitude;
-        else if (flags & kLoop)
-          output_buffer[i++] += src[1][ind % num_samples] * amplitude;
-        else
-          i++;
-
-        // Apply amplitude modification.
-        amplitude += amplitude_inc;
-        if (amplitude <= 0) {
-          remove = true;
-          break;
-        } else if (amplitude > max_amplitude) {
-          amplitude = max_amplitude;
-        }
-
-        // Basic resampling for variations.
-        accumulator += step;
-        src_index += accumulator / 10;
-        accumulator %= 10;
-
-        // Advance source index.
-        if (flags & kLoop) {
-          src_index %= num_samples;
-        } else if (src_index >= num_samples) {
-          remove = true;
-          break;
-        }
-      }
-
-      sample->src_index = src_index;
-      sample->accumulator = accumulator;
-      sample->amplitude = amplitude;
-    }
-
-    if (remove) {
-      sample->active = false;
-      it = samples_[1].erase(it);
-    } else {
-      ++it;
-    }
-  }
 }
 
 AudioOboe::StreamCallback::StreamCallback(AudioOboe* audio) : audio_(audio) {}
