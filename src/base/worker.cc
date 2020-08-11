532f1f287ce717e1fb385b2bf811d2ad9bca1502
Attila Uygun <auygun@opera.com>
2020-08-11 02:47:30 +0200

    Move threads from Worker to ThreadPool class.

--- src/base/worker.cc
+++ src/base/worker.cc
@@ -1,78 +1,118 @@
 #include "worker.h"
 
+#include <condition_variable>
+#include <deque>
+#include <mutex>
+#include <thread>
+#include <utility>
+#include <vector>
+
 #include "log.h"
+
+using Task = std::pair<base::Closure, base::Closure>;
+
+namespace {
+
+class ThreadPool {
+ public:
+  ThreadPool() = default;
+  ~ThreadPool() = default;
+
+  void Initialize(unsigned max_concurrency) {
+    if (max_concurrency > std::thread::hardware_concurrency() ||
+        max_concurrency == 0) {
+      max_concurrency = std::thread::hardware_concurrency();
+      if (max_concurrency == 0)
+        max_concurrency = 1;
+    }
+
+    while (max_concurrency--)
+      threads_.emplace_back(&ThreadPool::WorkerMain, this);
+  }
+
+  void Shutdown() {
+    {
+      std::unique_lock<std::mutex> scoped_lock(mutex_);
+      quit_when_idle_ = true;
+    }
+    cv_.notify_all();
+    for (auto& thread : threads_)
+      thread.join();
+    threads_.clear();
+  }
+
+  void Enqueue(Task task) {
+    DCHECK(!threads_.empty());
+
+    bool notify;
+    {
+      std::unique_lock<std::mutex> scoped_lock(mutex_);
+      notify = tasks_.empty();
+      tasks_.push_back(std::move(task));
+    }
+    if (notify)
+      cv_.notify_all();
+  }
+
+ private:
+  std::condition_variable cv_;
+  std::mutex mutex_;
+  std::vector<std::thread> threads_;
+  std::deque<Task> tasks_;
+  bool quit_when_idle_;
+
+  void WorkerMain() {
+    for (;;) {
+      std::pair<base::Closure, base::Closure> task;
+      {
+        std::unique_lock<std::mutex> scoped_lock(mutex_);
+        while (tasks_.empty()) {
+          if (quit_when_idle_)
+            return;
+          cv_.wait(scoped_lock);
+        }
+        task.swap(tasks_.front());
+        tasks_.pop_front();
+      }
+
+      task.first();
+      task.second();
+    }
+  }
+};
+
+ThreadPool g_thread_pool;
+
+}  // namespace
 
 namespace base {
 
-std::condition_variable Worker::cv_;
-std::mutex Worker::mutex_;
-std::vector<std::thread> Worker::threads_;
-std::deque<std::pair<base::Closure, base::Closure>> Worker::tasks_;
-bool Worker::quit_when_idle_ = false;
+Worker::Worker() = default;
+
+Worker::~Worker() {
+  DCHECK(lock_.load(std::memory_order_acquire) == 0);
+}
 
 void Worker::Initialize(unsigned max_concurrency) {
-  if (max_concurrency > std::thread::hardware_concurrency() ||
-      max_concurrency == 0) {
-    max_concurrency = std::thread::hardware_concurrency();
-    if (max_concurrency == 0)
-      max_concurrency = 1;
-  }
-
-  while (max_concurrency--)
-    threads_.emplace_back(&Worker::WorkerMain);
+  g_thread_pool.Initialize(max_concurrency);
 }
 
 void Worker::Shutdown() {
-  {
-    std::unique_lock<std::mutex> scoped_lock(mutex_);
-    quit_when_idle_ = true;
-  }
-  cv_.notify_all();
-  for (auto& thread : threads_)
-    thread.join();
-  threads_.clear();
+  g_thread_pool.Shutdown();
 }
 
 void Worker::Enqueue(base::Closure task) {
   DCHECK(task);
-  DCHECK(!threads_.empty());
 
   lock_.fetch_add(1, std::memory_order_relaxed);
-
-  bool notify;
-  {
-    std::unique_lock<std::mutex> scoped_lock(mutex_);
-    notify = tasks_.empty();
-    tasks_.emplace_back(std::make_pair(std::move(task), [&]() -> void {
-      lock_.fetch_sub(1, std::memory_order_release);
-    }));
-  }
-  if (notify)
-    cv_.notify_all();
+  g_thread_pool.Enqueue(std::make_pair(std::move(task), [&]() -> void {
+    lock_.fetch_sub(1, std::memory_order_release);
+  }));
 }
 
 void Worker::Join() {
   while (lock_.load(std::memory_order_acquire))
     ;  // spin
-}
-
-void Worker::WorkerMain() {
-  for (;;) {
-    std::pair<base::Closure, base::Closure> task;
-    {
-      std::unique_lock<std::mutex> scoped_lock(mutex_);
-      while (tasks_.empty()) {
-        if (quit_when_idle_)
-          return;
-        cv_.wait(scoped_lock);
-      }
-      task.swap(tasks_.front());
-      tasks_.pop_front();
-    }
-
-    task.first();
-    task.second();
-  }
 }
 
 }  // namespace base
--- src/base/worker.h
+++ src/base/worker.h
@@ -2,12 +2,7 @@
 #define WORKER_H
 
 #include <atomic>
-#include <condition_variable>
-#include <deque>
-#include <mutex>
-#include <thread>
-#include <utility>
-#include <vector>
+
 #include "closure.h"
 
 namespace base {
@@ -15,8 +10,8 @@ namespace base {
 // Feed the worker tasks and they will be called on a thread from the pool.
 class Worker {
  public:
-  Worker() = default;
-  ~Worker() = default;
+  Worker();
+  ~Worker();
 
   static void Initialize(unsigned max_concurrency = 0);
   static void Shutdown();
@@ -26,14 +21,6 @@ class Worker {
 
  private:
   std::atomic<int> lock_ = 0;
-
-  static std::condition_variable cv_;
-  static std::mutex mutex_;
-  static std::vector<std::thread> threads_;
-  static std::deque<std::pair<base::Closure, base::Closure>> tasks_;
-  static bool quit_when_idle_;
-
-  static void WorkerMain();
 
   Worker(Worker const&) = delete;
   Worker& operator=(Worker const&) = delete;
