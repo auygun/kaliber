ce6941cc20adf6f40690376fe3a742de206ef3f8
Attila Uygun <auygun@opera.com>
2025-09-17 13:02:45 +0200

    meshoptimizer

--- src/engine/model.cc
+++ src/engine/model.cc
@@ -8,6 +8,7 @@
 #include "engine/engine.h"
 #include "engine/platform/asset_file.h"
 #include "engine/renderer/renderer.h"
+#include "third_party/meshoptimizer/meshoptimizer.h"
 #include "third_party/tiny_obj_loader/tiny_obj_loader.h"
 
 namespace eng {
@@ -113,18 +114,41 @@ bool Model::LoadObj(Renderer* renderer, const std::string& file_name) {
     // int material_id = mi.first;
     auto& indices = mi.second;
 
+    // Optimize
+    meshopt_optimizeVertexCache(indices.data(), indices.data(), indices.size(),
+                                vertices.size());
+    meshopt_optimizeOverdraw(indices.data(), indices.data(), indices.size(),
+                             &vertices[0].position[0], vertices.size(),
+                             sizeof(Vertex), 1.05f);
+
+    std::vector<uint32_t> remap(indices.size());
+    size_t num_vertices = meshopt_generateVertexRemap(
+        remap.data(), indices.data(), indices.size(), vertices.data(),
+        vertices.size(), sizeof(Vertex));
+
+    std::vector<Vertex> new_vertices(num_vertices);
+    std::vector<uint32_t> new_indices(indices.size());
+
+    meshopt_remapVertexBuffer(new_vertices.data(), vertices.data(),
+                              vertices.size(), sizeof(Vertex), remap.data());
+    meshopt_remapIndexBuffer(new_indices.data(), indices.data(), indices.size(),
+                             remap.data());
+
+    // TODO: Update global vertex list once (first material)
+    vertices.swap(new_vertices);
+
     geometries_.emplace_back(renderer_);
     geometries_.back().Create(kPrimitive_Triangles, vertex_description_,
                               kDataType_UInt);
-    geometries_.back().Update(vertices.size(), vertices.data(), indices.size(),
-                              indices.data());
+    geometries_.back().Update(vertices.size(), vertices.data(),
+                              new_indices.size(), new_indices.data());
   }
 
   return true;
 }
 
 void Model::Draw() {
-  for (auto &g : geometries_)
+  for (auto& g : geometries_)
     g.Draw();
 }
 
